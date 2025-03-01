1371713d54386198fb25088ec2dc4b88842344f7
deep60 <deep6060p@gmail.com>
2025-03-01 22:39:39 +0530

    Add main.rs/ rust analyzer used

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod blockchain;
+mod config;
+mod security;
+mod ui;
+mod wallet;
+
+use config::AppConfig;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("Initializing SafeCoin Wallet...");
+
+    // Load configuration
+    let config = AppConfig::load()?;
+
+    //Intialize the wallet
+    let wallet_manager = wallet::WalletManager::new(&config)?;
+
+    Ok(())
 }
